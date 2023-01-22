0f46f0ef2fba1df1279705e013504cc01775fbbf
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2023-01-23 00:47:54 +0530

    Update BSTimplementation.cpp

--- Tree/BSTimplementation.cpp
+++ Tree/BSTimplementation.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -61,6 +53,70 @@ void inOrderTraversal(Node* root){
     inOrderTraversal(root->right);
 }
 
+Node* minVal(Node* root){
+    Node* temp=root;
+    
+    while(temp->left != NULL){
+        temp=temp->left;
+    }
+    return temp;
+}
+
+Node* maxVal(Node* root){
+    Node* temp=root;
+    
+    while(temp->right != NULL){
+        temp=temp->right;
+    }
+    return temp;
+}
+
+Node* deleteFromBST(Node* root, int key){
+    //base case
+    if(root==NULL){
+        return root;
+    }
+    
+    if(root->data == key){
+        //0 child
+        if(root->left==NULL && root->right==NULL){
+            delete root;
+            return NULL;
+        }
+        else if(root->left!=NULL && root->right==NULL){
+        //left child
+            Node* temp=root->left;
+            delete root;
+            return temp;
+        }
+        else if(root->left==NULL && root->right!=NULL){
+        //right child
+            Node* temp=root->right;
+            delete root;
+            return temp;
+        }
+        
+        //2 child
+        else{
+            int mini = minVal(root->right) -> data;
+            root -> data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+        
+    }
+    else{
+        if(key>root->data){
+            root->right = deleteFromBST(root->right, key);
+            return root;
+        }
+        else{
+            root->left = deleteFromBST(root->left, key);
+            return root;
+        }
+    }
+}
+
 int main()
 {
     Node* root=NULL;
@@ -70,6 +126,20 @@ int main()
     
     cout<<"printing inOrderTraversal of tree "<<"\n";
     inOrderTraversal(root);
+    cout<<"\n";
+    
+    cout<<"min value of the tree is "<<minVal(root) -> data <<endl;
+    cout<<"max value of the tree is "<<maxVal(root) -> data <<endl;
+    
+    //input for deleteing an node in the tree
+    //10 20 30 50 70 90 110 -1
+    root = deleteFromBST(root, 30);
+    
+    cout<<"printing inOrderTraversal of tree "<<"\n";
+    inOrderTraversal(root);
+    cout<<"\n";
+    
+    
 
     return 0;
 }
